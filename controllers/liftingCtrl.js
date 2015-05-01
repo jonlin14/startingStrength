7e7a7f78e7d9ddf0886425b2a60fe5f6ad4d599e
Jonathan Lin <johnlin0214@gmail.com>
2015-05-01 18:21:14 -0400

    Add loop for squat numbers

--- controllers/liftingCtrl.js
+++ controllers/liftingCtrl.js
@@ -1,3 +1,12 @@
 myApp.controller('liftingCtrl', function liftingCtrl($scope) {
-  $scope.name = name;
+  $scope.squatArray = [];
+  $scope.deadliftArray = [];
+  $scope.militaryPressArray = [];
+
+  $scope.addNumbers = function() {
+    $scope.squatArray.push($scope.squat);
+    $scope.deadliftArray.push($scope.deadlift);
+    $scope.militaryPressArray.push($scope.militaryPress);
+  }
+
 });
